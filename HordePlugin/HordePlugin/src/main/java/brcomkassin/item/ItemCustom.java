885895418f4318b668842e95d01fd6016852d1d9
KassioSantoss <136283828+KassioSantoss@users.noreply.github.com>
2024-06-27 16:43:10 -0300

    Add files via upload

---
+++ HordePlugin/HordePlugin/src/main/java/brcomkassin/item/ItemCustom.java
@@ -0,0 +1,9 @@
+package brcomkassin.item;
+
+public class ItemCustom extends ItemCustomType {
+
+    static {
+       HORDE_ITEM = ItemMetaManager.setItemMetaData(HORDE_ITEM, "horde-sword");
+    }
+
+}
