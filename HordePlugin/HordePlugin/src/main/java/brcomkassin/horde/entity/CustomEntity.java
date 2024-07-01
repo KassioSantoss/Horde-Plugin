ec4219bd5001a57ec7991975d7d4a117070d2e11
KassioSantoss <136283828+KassioSantoss@users.noreply.github.com>
2024-07-01 15:58:10 -0300

    Delete HordePlugin/HordePlugin directory

--- HordePlugin/HordePlugin/src/main/java/brcomkassin/horde/entity/CustomEntity.java
+++
@@ -1,9 +0,0 @@
-package brcomkassin.horde.entity;
-
-import org.bukkit.Location;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-
-public interface CustomEntity {
-    LivingEntity spawnEntity(CustomEntityType customEntityType , Location location);
-}
